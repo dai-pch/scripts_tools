e28b5148da566c848ea5ca07e0925a7e047de7ba
Dai, Pengcheng <dpc_work@163.com>
2019-06-25 16:47:53 +0800

    Returen 0 if relative number is zero.

--- stats_parser.py
+++ stats_parser.py
@@ -124,7 +124,7 @@ def proc_normal(data):
         if not ref:
             print("Warning: Can not find normal ref ", str(ref_path), " for ", node.name)
             continue
-        node.data = float(real_node.data) / float(ref.data)
+        node.data = float(real_node.data) / float(ref.data) if 0 !=ref.data else (0 if float(real_node.data) == 0 else float(Nan))
 
 def gen_blk(v, row_n, ws):
     # scan col item
